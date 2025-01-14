4a97ba85a2cf5a836446a412f480c118a63eb78f
Lara <laravegas00@gmail.com>
2025-01-14 18:12:11 +0100

    Botón Sign in con funcionalidad

--- app/src/main/java/edu/pmdm/vegas_laraimdbapp/MainActivity.java
+++ app/src/main/java/edu/pmdm/vegas_laraimdbapp/MainActivity.java
@@ -1,11 +1,17 @@
 package edu.pmdm.vegas_laraimdbapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -48,6 +54,44 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        // Obtener datos del intent
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String email = intent.getStringExtra("email");
+        String photoUrl = intent.getStringExtra("photoUrl");
+
+        // Actualizar UI del encabezado del Navigation Drawer
+        View headerView = navigationView.getHeaderView(0);
+        TextView nameTextView = headerView.findViewById(R.id.nav_header_name);
+        TextView emailTextView = headerView.findViewById(R.id.nav_header_email);
+        ImageView photoImageView = headerView.findViewById(R.id.nav_header_image);
+
+        nameTextView.setText(name);
+        emailTextView.setText(email);
+
+        if (photoUrl != null) {
+            Glide.with(this).load(photoUrl).into(photoImageView);
+        } else {
+            photoImageView.setImageResource(R.drawable.googlelogo); // Imagen predeterminada si no hay foto
+        }
+
+        Button logoutButton = headerView.findViewById(R.id.nav_header_logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cerrar sesión
+                FirebaseAuth.getInstance().signOut();
+                // Volver a la pantalla de inicio de sesión
+                Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 
     @Override
